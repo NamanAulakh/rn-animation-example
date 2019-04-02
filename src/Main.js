09899a4cc5f4793d073a8c3db7c11e70abd51afb
Naman <aulakh.naman@gmail.com>
2019-04-02 12:29:58 +0530

    temp changes

---
+++ src/Main.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import {
+  Dimensions,
+  PanResponder,
+  View,
+  Animated,
+  StyleSheet,
+  Text,
+  Button,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+const styles = StyleSheet.create({
+  container1: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  container2: {
+    // flex: 1,
+    backgroundColor: 'red',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginHorizontal: 10,
+    borderRadius: 10,
+  },
+});
+const deviceHeight = Dimensions.get('window').height;
+
+export default class Main extends Component {
+  render() {
+    return <Icon name="rocket" size={30} color="#900" />;
+  }
+}
